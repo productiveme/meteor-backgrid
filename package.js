c14f6d8df27201d19ac333d9cf7755634a070c59
productiveme <jaco@productive.me>
2015-02-13 10:27:44 +0200

    Updates to package.js for publishing

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'backgrid',
+  name: 'productiveme:backrid',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Backgrid.js is a set of components for building semantic and easily stylable data grid widgets.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/productiveme/meteor-backgrid.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
